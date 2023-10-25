1345779aa0d586e4b4887bd2cb6cb69691754fa5
Paiva <danpaiva@live.com>
2023-10-25 15:21:39 -0300

    feat: add program and database

---
+++ ExampleSqlLiteNet/sqlite/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Sqlite;
+
+try
+{
+  using (var connection = new SqliteConnection("Data Source=C:\\banco_loja\\DB_LOJA.db"))
+  {
+    connection.Open();
+
+    var command = connection.CreateCommand();
+    command.CommandText =
+    @"
+        SELECT name
+        FROM tb_user
+    ";
+    command.Parameters.AddWithValue("$id", 1);
+
+    using (var reader = command.ExecuteReader())
+    {
+      while (reader.Read())
+      {
+        var name = reader.GetString(0);
+
+        Console.WriteLine($"Hello, {name}!");
+      }
+    }
+  }
+
+}
+catch (System.Exception erro)
+{
+  Console.WriteLine($"Deu um erro no banco: {erro.Message}");
+}
+finally
+{
+  Console.ReadLine();
+}
